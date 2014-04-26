6453d338ba7f58bd1ae8c7d3d724a9eec527acd2
Brett Cannon <brett@python.org>
2014-04-26 19:03:41 -0400

    Add some temporary example people

--- web_app/trips_history/web/trips_ui.dart
+++ web_app/trips_history/web/trips_ui.dart
@@ -10,6 +10,16 @@ class TripsHistoryController {
   String personName;
   String personColour;
 
+  TripsHistoryController() {
+    // TODO: remove example people.
+    var a = new Person('Andrea', '#EEEE00');
+    var b = new Person('Brett', '#0000EE');
+    var ab = new Person('Andrea & Brett', '#00EE00');
+    people.add(a);
+    people.add(b);
+    people.add(ab);
+  }
+
   void addPerson() {
     var person = new Person(personName, personColour);
     people.add(person);
