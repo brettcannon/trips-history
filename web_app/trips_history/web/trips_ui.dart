0fc964ccc8a4a44023de69b9d3339586e10a0def
Brett Cannon <brett@python.org>
2014-04-27 11:37:24 -0400

    Add support for saving a city

--- web_app/trips_history/web/trips_ui.dart
+++ web_app/trips_history/web/trips_ui.dart
@@ -6,18 +6,33 @@ import 'trips_history.dart';
     selector: '[trips-history]',
     publishAs: 'ctrl')
 class TripsHistoryController {
+  // Person
   List<Person> people = new List();
   String personName;
   String personColour;
+  // City
+  String cityName;
+  String cityCountryCode;
+  String cityLongitude;
+  String cityLatitude;
+  bool cityLivedIn;
+  List<City> cities = new List();
 
   TripsHistoryController() {
-    // TODO: remove example people.
+    // TODO: remove example data.
     var a = new Person('Andrea', '#EEEE00');
     var b = new Person('Brett', '#0000EE');
     var ab = new Person('Andrea & Brett', '#00EE00');
     people.add(a);
     people.add(b);
     people.add(ab);
+
+    var vancouver = new City('Vancouver', 'CA',
+        longitude: -123.11934, latitude: 49.24966, livedHere:true);
+    var buenosAires = new City('Buenos Aires', 'AR',
+        longitude: -58.37723, latitude: -34.61315);
+    cities.add(vancouver);
+    cities.add(buenosAires);
   }
 
   void addPerson() {
@@ -25,6 +40,19 @@ class TripsHistoryController {
     people.add(person);
     personName = '';
     personColour = '';
+  }
+
+  void addCity() {
+    var city = new City(cityName, cityCountryCode,
+        longitude: double.parse(cityLongitude),
+        latitude: double.parse(cityLatitude),
+        livedHere: cityLivedIn);
+    cities.add(city);
+    cityName = '';
+    cityCountryCode = '';
+    cityLatitude = '';
+    cityLongitude = '';
+    cityLivedIn = false;
   }
 }
 
